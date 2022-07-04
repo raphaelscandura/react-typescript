c300290f03c46e5c862f947052cab60bee2ef5f8
Raphael Scandura <raphaelscandura@hotmail.com>
2022-07-04 17:38:14 -0300

    List with dynamic mock data

--- projeto/src/components/lists/index.tsx
+++ projeto/src/components/lists/index.tsx
@@ -1,29 +1,31 @@
 import React from "react";
 
 function List(){
+    const tasks=[{
+        task: "React",
+        time: "02:00:00"
+    },{
+      task: "Javascript",
+      time: "01:00:00"  
+    }];
+
     return (
         <aside>
             <h2>Today's studies</h2>
             <ul>
-                <li>
-                    <h3>
-                        React
-                    </h3>
-                    <span>
-                        02:00:00
-                    </span>
-                </li>
-                <li>
-                    <h3>
-                        Javascript
-                    </h3>
-                    <span>
-                        01:00:00
-                    </span>
-                </li>
+                {tasks.map((item,index)=>(
+                    <li key={index}>
+                        <h3>
+                            {item.task}
+                        </h3>
+                        <span>
+                            {item.time}
+                        </span>
+                    </li>
+                ))}
             </ul>
         </aside>
-    )
+    );
 }
 
 export default List;
