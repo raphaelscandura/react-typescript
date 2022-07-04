0d2a99ad97fd96dc73ceeeda06d101360a7e5ec6
Raphael Scandura <raphaelscandura@hotmail.com>
2022-07-04 17:29:03 -0300

    List component function

--- projeto/src/App.tsx
+++ projeto/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Form from './components/forms';
+import List from './components/lists';
 
 function App() {
   return (
     <div className="App">
       <Form/>
+      <List/>
     </div>
   );
 }
