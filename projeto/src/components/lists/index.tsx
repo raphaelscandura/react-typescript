bbf3651cfb824e24a4db32180ed76cbadf3042e0
raphaelscandura <raphaelscandura@hotmail.com>
2022-07-05 09:26:53 -0300

    Testing useState function

--- projeto/src/components/lists/index.tsx
+++ projeto/src/components/lists/index.tsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./item";
 import style from './style.module.scss';
 
 function List(){
-    const tasks=[{
+    const [tasks,setTasks]= useState([{
         task: "React",
         time: "02:00:00"
     },{
       task: "Javascript",
       time: "01:00:00"  
-    }];
+    }]);
 
     return (
         <aside className={style.taskList}>
-            <h2>Today's studies</h2>
+            <h2 onClick={() => {
+                setTasks([...tasks,{task:"Test",time:"04:00:00"}])
+            }}>Today's studies</h2>
             <ul>
                 {tasks.map((item,index)=>(
                     <Item 
