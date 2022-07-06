8184194fac61626c48bc30cc2d3b7b4d109968d7
raphaelscandura <raphaelscandura@hotmail.com>
2022-07-06 12:51:28 -0300

    Project completed

--- React - Lidando com arquivos estáticos/src/pages/menu/items/index.tsx
+++ React - Lidando com arquivos estáticos/src/pages/menu/items/index.tsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from 'react';
 
 export default function Items(props:ItemInterface){
     const [list,setList] = useState(items);
-    const {search,filter} = props;
+    const {search,filter,ordering} = props;
 
     function findSearch(title:string){
         const regex=new RegExp(search,'i');
@@ -19,10 +19,23 @@ export default function Items(props:ItemInterface){
         return true;
     }
 
+    function orderingList(newList: typeof items){
+        switch(ordering){
+            case 'portion':
+                return newList.sort((a,b)=>a.size>b.size?1:-1)
+            case 'ppl_amount':
+                return newList.sort((a,b)=>a.serving>b.serving?1:-1)
+            case 'price':
+                return newList.sort((a,b)=>a.price>b.price?1:-1)
+            default:
+                return newList;
+        }
+    }
+
     useEffect(()=>{
         const newList = items.filter(item => findSearch(item.title) && findFilter(item.category.id))
-        setList(newList);
-    },[search,filter])
+        setList(orderingList(newList));
+    },[search,filter,ordering])
     return (
         <div className={styles.items}>
             {list.map(item => (
