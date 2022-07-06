e0626369d47c20689769c7c3420538d05ff1f910
Raphael Scandura <raphaelscandura@hotmail.com>
2022-07-06 19:55:18 -0300

    Final refactor and project conclusion

--- React - Conhecendo a biblioteca React Router/src/pages/plate/index.tsx
+++ React - Conhecendo a biblioteca React Router/src/pages/plate/index.tsx
@@ -1,8 +1,9 @@
 import styles from './plate.module.scss';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Route, Routes } from 'react-router-dom';
 import foodmenu from 'data/foodmenu.json';
 import PlateTags from 'components/platetags';
 import NotFound from 'pages/notfound';
+import Template from 'pages/template';
 
 export default function Plate(){
 	const navigate=useNavigate();
@@ -10,27 +11,33 @@ export default function Plate(){
 	const plate=foodmenu.find(item=>item.id===Number(id));
 	if(!plate) return <NotFound/>;
 	return (
-		<>
-			<button 
-				className={styles.goback}
-				onClick={()=>navigate(-1)}
-			>
-				{'< Go back'}
-			</button>
-			<section className={styles.container}>
-				<h1 className={styles.title}>
-					{plate.title}
-				</h1>
-				<div>
-					<img src={plate.photo} alt={plate.title} />
-				</div>
-				<div className={styles.content}>
-					<p className={styles.content_description}>
-						{plate.description}
-					</p>
-					<PlateTags {...plate}/>
-				</div>
-			</section>
-		</>
+		<Routes>
+			<Route path="*" element={<Template/>}>
+				<Route index element={
+					<>
+						<button 
+							className={styles.goback}
+							onClick={()=>navigate(-1)}
+						>
+							{'< Go back'}
+						</button>
+						<section className={styles.container}>
+							<h1 className={styles.title}>
+								{plate.title}
+							</h1>
+							<div>
+								<img src={plate.photo} alt={plate.title} />
+							</div>
+							<div className={styles.content}>
+								<p className={styles.content_description}>
+									{plate.description}
+								</p>
+								<PlateTags {...plate}/>
+							</div>
+						</section>
+					</>
+				}/>
+			</Route>
+		</Routes>
 	);
 }
