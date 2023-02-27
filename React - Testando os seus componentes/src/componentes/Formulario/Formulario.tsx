5590daef81be0c9e08bdac93ce3b03ada583fcf3
raphaelscandura <raphaelscandura@hotmail.com>
2023-02-27 14:22:27 -0300

    adding new members on the list and testing it

--- React - Testando os seus componentes/src/componentes/Formulario/Formulario.tsx
+++ React - Testando os seus componentes/src/componentes/Formulario/Formulario.tsx
@@ -1,7 +1,30 @@
+import { useRef, useState } from "react"
+import { useAdicionarParticipante } from "../../state/hooks/useAdicionarParticipante"
+
 const Formulario=()=>{
-    return (<form>
-        <input type="text" placeholder="Insira os nomes dos participantes"/>
-        <button disabled={true}>Adicionar</button>
+
+    const [nome,setNome]=useState('')
+
+    const inputRef=useRef<HTMLInputElement>(null)
+
+    const adicionar=useAdicionarParticipante()
+
+    const adicionarParticipante=(evento:React.FormEvent<HTMLFormElement>)=>{
+        evento.preventDefault()
+        adicionar(nome)
+        setNome('')
+        inputRef.current?.focus()
+    }
+
+    return (<form onSubmit={adicionarParticipante}>
+        <input 
+            ref={inputRef}
+            value={nome}
+            onChange={evento=>setNome(evento.target.value)}
+            type="text" 
+            placeholder="Insira os nomes dos participantes"
+        />
+        <button disabled={!nome}>Adicionar</button>
     </form>)
 }
 
--- React - Testando os seus componentes/src/componentes/Formulario/Forumlario.test.tsx
+++ React - Testando os seus componentes/src/componentes/Formulario/Forumlario.test.tsx
@@ -1,10 +1,13 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
+import { RecoilRoot } from 'recoil';
 import Formulario from "./Formulario";
 
 test('se o imput estiver vazio, novos participantes não podem ser adicionados',()=>{
     //renderizar o componente em questão
-    render(<Formulario/>)
+    render(<RecoilRoot>
+            <Formulario/>
+        </RecoilRoot>)
 
     //encontrar o input no DOM
     const input=screen.getByPlaceholderText('Insira os nomes dos participantes')
@@ -17,6 +20,33 @@ test('se o imput estiver vazio, novos participantes não podem ser adicionados',
 
     //garantir que o botão esteja desabilitado
     expect(botao).toBeDisabled()
+})
 
+test('adicionar um participante caso exista um nome preenchido',()=>{
+        //renderizar o componente em questão
+        render(<RecoilRoot>
+                <Formulario/>
+            </RecoilRoot>)
 
+        //encontrar o input no DOM
+        const input=screen.getByPlaceholderText('Insira os nomes dos participantes')
+    
+        //encontrar o botão de submit
+        const botao=screen.getByRole('button')
+
+        //inserir um valor no input
+        fireEvent.change(input,{
+            target:{
+                value: 'nome de teste'
+            }
+        })
+
+        //clicar no botão de submeter
+        fireEvent.click(botao)
+
+        //garantir que o input esteja com o foco ativo
+        expect(input).toHaveFocus()
+
+        //garantir que o input não tenha um valor
+        expect(input).toHaveValue("")
 })
