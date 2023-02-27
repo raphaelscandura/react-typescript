b07c30438da55b49615219ea2dab0a8b68cbe438
raphaelscandura <raphaelscandura@hotmail.com>
2023-02-27 12:37:09 -0300

    fetching data from mock api/server

--- React - Gerenciando estado com Recoil/src/state/seletores/index.ts
+++ React - Gerenciando estado com Recoil/src/state/seletores/index.ts
@@ -1,4 +1,5 @@
-import{selector}from"recoil";
+import {selector} from "recoil";
+import { IEvento } from "../../interfaces/IEvento";
 import{filtroDeEventos,listaDeEventosState}from"../atom";
 
 export const eventosFiltradosState=selector({
@@ -6,6 +7,19 @@ export const eventosFiltradosState=selector({
   get:({get})=>{
     const filtro=get(filtroDeEventos)
     const todosOsEventos=get(listaDeEventosState)
-	return todosOsEventos.filter(evento => (filtro.data?filtro.data.toISOString().slice(0,10)===evento.inicio.toISOString().slice(0,10):true))
+	return todosOsEventos.filter((evento: { inicio: { toISOString: () => string | any[]; }; }) => (filtro.data?filtro.data.toISOString().slice(0,10)===evento.inicio.toISOString().slice(0,10):true))
  }
+})
+
+export const eventosAsync = selector({
+  key: 'eventosAsync',
+  get: async()=>{
+    const respostaHttp=await fetch('http://localhost:8080/eventos')
+    const eventosJson: IEvento[] = await respostaHttp.json()
+    return eventosJson.map(evento => ({
+      ...evento,
+      inicio: new Date(evento.inicio),
+      fim: new Date(evento.fim)
+    }))
+  }
 })
