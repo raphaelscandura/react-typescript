5934e301d42b78fa0e24014d9de6fa8f8a1020da
raphaelscandura <raphaelscandura@hotmail.com>
2023-02-28 09:57:57 -0300

    config page plus testing (snapshot)

---
+++ React - Testando os seus componentes/src/paginas/Configuracao/Configuracao.tsx
@@ -0,0 +1,16 @@
+import Formulario from "../../componentes/Formulario/Formulario"
+import ListaParticipantes from "../../componentes/ListaParticipantes/ListaParticipantes"
+import Rodape from "../../componentes/Rodape/Rodape"
+
+const Configuracao=()=>{
+    return(
+        <>
+            <Formulario/>
+            <ListaParticipantes/>
+            <Rodape/>
+        </>
+    )
+
+}
+
+export default Configuracao
