342e14b1bf0e91c91753c939b8d3c10b32d650c5
raphaelscandura <raphaelscandura@hotmail.com>
2023-04-24 10:27:20 -0300

    new pages

--- React - otimizando a performance/src/pages/Cardapio/Buscador/index.tsx
+++ React - otimizando a performance/src/pages/Cardapio/Buscador/index.tsx
@@ -1,20 +1,24 @@
-import React from "react";
-import styles from "./Buscador.module.scss";
-import { CgSearch } from "react-icons/cg";
+import React, { memo, useMemo } from 'react';
+import styles from './Buscador.module.scss';
+import { CgSearch } from 'react-icons/cg';
 
 interface Props {
   busca: string;
   setBusca: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default function Buscador({ busca, setBusca }: Props) {
+function Buscador({ busca, setBusca }: Props) {
+  const elemento = useMemo(() => <CgSearch size={20} color="#4C4D5E" />, []);
   return (
     <div className={styles.buscador}>
       <input
         value={busca}
         onChange={(evento) => setBusca(evento.target.value)}
+        placeholder="Buscar"
       />
-      <CgSearch size={20} color="#4C4D5E" />
+      {elemento}
     </div>
   );
 }
+
+export default memo(Buscador);
